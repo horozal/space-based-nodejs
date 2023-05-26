9121b9b3bb187e86cdd987f589b272e7aa727fdb
horozal <36187605+horozal@users.noreply.github.com>
2023-05-26 12:45:14 +0200

    Create index.js

---
+++ src/data-grid/index.js
@@ -0,0 +1,8 @@
+const InMemoryGrid = require('./InMemoryGrid');
+
+// Create an instance of the in-memory data grid
+const dataGrid = new InMemoryGrid();
+
+// Example usage of the in-memory data grid
+dataGrid.put('key1', 'value1');
+dataGrid.put('key2', 'value2');
