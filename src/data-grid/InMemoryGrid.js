91d58c412e28b485f21fb78b75b20ef7b6f5268a
horozal <36187605+horozal@users.noreply.github.com>
2023-05-26 12:43:52 +0200

    Create InMemoryGrid.js

---
+++ src/data-grid/InMemoryGrid.js
@@ -0,0 +1,7 @@
+class InMemoryGrid {
+  constructor() {
+    this.data = new Map();
+    // Initialize any necessary state or dependencies for the in-memory grid
+  }
+  
+  module.exports = InMemoryGrid;
