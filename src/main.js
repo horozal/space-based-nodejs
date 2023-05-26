6cc9c87dfec9cdf48eaa4b7d31cbfd79109a18b8
horozal <36187605+horozal@users.noreply.github.com>
2023-05-26 12:47:46 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
-var http = require('http');
+const Component = require('./components/Component');
+const Space = require('./spaces/Space');
+const InMemoryGrid = require('./data-grid/InMemoryGrid');
+
+// Create instances of components, spaces, and data grid
+const component = new Component();
+const space = new Space();
+const dataGrid = new InMemoryGrid();
+
+const http = require('http');
 
 http.createServer(function (req, res) {
   res.write('Hello World!');
