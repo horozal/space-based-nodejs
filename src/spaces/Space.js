79d194ba5cba15160275aaf6f1b0864a970d5d61
horozal <36187605+horozal@users.noreply.github.com>
2023-05-26 13:59:41 +0200

    Update Space.js

--- src/spaces/Space.js
+++ src/spaces/Space.js
@@ -1,10 +1,9 @@
-const OrderComponent = require('../components/Component');
+const Component = require('../components/Component');
 
-class OrderSpace {
+class Space {
   constructor() {
     this.component = new Component();
     // Initialize any necessary state or dependencies related to the space
   }
-
 
 module.exports = Space;
